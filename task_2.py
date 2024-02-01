a7f88a5665412d4f71deb55fc8ac8f6296baad95
NazarS11 <bamperor@gmail.com>
2024-02-01 02:33:33 +0200

    v1.1

--- task_2.py
+++ task_2.py
@@ -4,13 +4,17 @@ def get_cats_info(path):
     list = []
     try:
         with open(path, 'r', encoding='utf-8') as fh:
+            #Create 'lines' list adding each line from 'path' file, cutting spaces from both sides of each line
             lines = [el.strip() for el in fh.readlines()]
             if lines:
                 for line in lines:               
+                    #Create list with dictionary elements adding from lines list using ',' separator
                     list.append({"id":((line.split(','))[0]),"name":(line.split(','))[1],"age":(line.split(','))[2]})
                 return(list)
+            #Error message in case if file is empty
             print(f"Calculating error: {path} file is empty")
             return sys.exit()
+    #Handling cases when a file is missing or damaged
     except Exception as e:
         print(f"File accessing error: {e}")
         return sys.exit()
